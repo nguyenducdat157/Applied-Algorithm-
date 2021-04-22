f48c20e41215a5cf67852b221c54cb4ed1796ade
ashley <ashley@trueplus.vn>
2021-04-22 21:09:02 +0700

    update machine problem

--- MACHINE_DP.cpp
+++ MACHINE_DP.cpp
@@ -10,7 +10,7 @@ long ans = -1;
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
     cout.tie(0);
-    freopen("/home/ashley/Documents/Hust/Applied-Algorithm/Repo/0.INP", "r", stdin);
+    //freopen("/home/ashley/Documents/Hust/Applied-Algorithm/Repo/0.INP", "r", stdin);
 
     memset(dp, -1, sizeof (dp));
     cin >> n;
@@ -22,13 +22,9 @@ int main() {
         dp[t[i]] = max(dp[t[i]], t[i] - s[i]);
     }
 
-    for (long i = 1; i < n; i++) {
+    for (long i = 1; i < MAX; i++) {
         dp[i] = max(dp[i-1], dp[i]);
     }
-
-    for (int i = 1; i <= 12; i++) {
-        cout << dp[i] << " ";
-    }return 0;
 
     for (long i = 1; i <= n; i++) {
         if (dp[s[i] - 1] > 0) {
