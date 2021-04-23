db1d6751670e259aec2514e00fdb896de36fdc80
ashley <ashley@trueplus.vn>
2021-04-24 00:29:09 +0700

    add some problem

--- SWEET.cpp
+++ SWEET.cpp
@@ -1,57 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
-#define N 15
 
-int n, K;
-bool mark[N];
-int temp[N];
-bool p[N][N];
-int cnt = 0;
+const long MOD = 1e9 + 7;
+const int MAX = 100;
+int n, k;
+int dp[MAX][MAX];
 
-void print() {
-    cnt++;
-    for (int i = 1; i <= n; i++) {
-        cout << temp[i] << " ";
-    }
-    cout << endl;
-}
-
-bool check(int i, int k) {
-    if (mark[i]) return false;
-    for (int j = 1; j < k; j++) {
-        if (p[i][temp[j]]) return false;
-    }
-    return true;
-}
-
-void Try(int k) {
-    for (int i = 1; i <= n; i++) {
-        if (check(i, k)) {
-            temp[k] = i;
-            mark[i] = true;
-            if (k == n) {
-                print();
-            } else Try(k + 1);
-            mark[i] = false;
+int C(int k, int n) {
+    if (k == 0 || k == n) return 1;
+    if (k == 1) return n;
+    for (int i = 0; i <= k; i++) {
+        for (int j = i; j <= n; j++) {
+            if (i == 0 || i == j) dp[i][j] = 1;
+            else dp[i][j] = (dp[i][j - 1] % MOD + dp[i - 1][j - 1] % MOD) % MOD;
         }
     }
+    return dp[k][n];
 }
 
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    //freopen("/home/ashley/Documents/Hust/Applied-Algorithm/Repo/0.INP", "r", stdin);
-    cin >> n >> K;
-
-    int u, v;
-    for (int i = 1; i <= K; i++) {
-        cin >> u >> v;
-        p[u][v] = true;
-    }
-    Try(1);
-
-    cout << cnt;
-    return 0;
+    cin >> n >> k;
+    cout << C(n, n + k - 1);
 }
